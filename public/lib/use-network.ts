1c3afcaf48cdbb6531dd3b2cadc125e373c3f333
Otis Sutton <otis@myvirtualmission.com>
2023-01-25 12:07:30 +1300

    More generic (and typed) promise hook

--- public/lib/use-network.ts
+++ public/lib/use-network.ts
@@ -1,74 +1,30 @@
-import PocketBase from 'pocketbase';
-
 import { useState } from 'preact/hooks';
-
-const PB = new PocketBase('https://api.coffeeandcode.app');
 
 const CACHE = new Map();
 
-const load = async (url: string) => {
-	const res = await fetch(url);
-	const data = await res.json();
-	return data;
-}
+export const usePromise = <T = any>(key: string, handler: (key?: string) => Promise<T>) => {
+	type ResolvedValue = Awaited<ReturnType<typeof handler>>;
+	
+	const [_, update] = useState<unknown>(null);
 
-// @NOTE
-// - Get around pre-render issue
-// - https://github.com/preactjs/wmr/discussions/950
-export const useFetch = <T = any>(url: string): T => {
-	const [_, update] = useState<unknown>(0);
-	let value = CACHE.get(url);
+	let value = CACHE.get(key);
 
+	if (value === undefined) {
+		value = handler(key);
 
-	if (!value) {
-		value = load(url);
-		CACHE.set(url, value);
-		// @TODO
-		// - Come up with something better than this...
+		CACHE.set(key, value);
+
 		value.then(
-			(res: unknown) => update((value.res = res)),
-			(rej: unknown) => update((value.rej = rej))
+			(res: ResolvedValue) => update((value.res = res)),
 		);
 	}
 
 	if (value.res) {
-		CACHE.delete(url);
-		return value.res
+		return value.res as ResolvedValue;
 	};
 
-	if (value.rej) {
-		CACHE.delete(url);
-		throw value.rej
-	};
 
 	throw value;
 }
 
-export const usePocketBase = <T = any>(collection: string): T[] => {
-	const [_, update] = useState<unknown>(0);
-	let value = CACHE.get(collection);
-
-	if (!value) {
-		value = PB.collection(collection).getFullList(200, {
-			sort: '-created',
-		});
-		CACHE.set(collection, value);
-		value.then(
-			(res: unknown) => update((value.res = res)),
-			(rej: unknown) => update((value.rej = rej))
-		);
-	}
-
-	if (value.res) {
-		CACHE.delete(collection);
-		return value.res
-	};
-
-	if (value.rej) {
-		CACHE.delete(collection);
-		throw value.rej
-	};
-
-	throw value;
-}
 
--- public/pages/home/index.tsx
+++ public/pages/home/index.tsx
@@ -1,17 +1,22 @@
+import PocketBase from 'pocketbase';
 import styles from './style.module.css';
-import { useState } from 'preact/hooks';
-import { usePocketBase } from '../../lib/use-network';
 
+import { useState } from 'preact/hooks';
+import { usePromise } from '../../lib/use-network';
+
+const PB = new PocketBase('https://api.coffeeandcode.app');
 export default function Home() {
 	const [count, setCount] = useState(0);
-	const records = usePocketBase<{ audio: string }>('tracks');
+	const records = usePromise('tracks', () => PB.collection('tracks').getFullList<{ audio: string }>(200, {
+		sort: '-created',
+	}));
 
 	return (
 		<>
 			<section class={styles.home}>
 				<h1>Home</h1>
 				<p>This is the home page.</p>
-	{records.map(record => <p>{record.audio}</p>)}
+				{records.map(record => <p>{record.audio}</p>)}
 				<>
 					<button style={{ width: 30 }} onClick={() => setCount(count - 1)}>
 						-
