08661f7de6a74972f765062d77b7d57cdce742d8
Otis Sutton <otis@Clares-MacBook-Pro.local>
2023-02-17 20:37:52 +1300

    Add new font

--- public/index.js
+++ public/index.js
@@ -2,7 +2,21 @@ import withTwind from '@twind/wmr';
 
 import { App } from './app';
 
-const { hydrate, prerender } = withTwind((data) => <App {...data} />)
+const { hydrate, prerender } = withTwind({
+  theme: {
+    extend: {
+      fontFamily: (theme) => {
+        return {
+          sans: ['Inconsolata', ...theme('fontFamily').sans],
+        }
+      }
+    },
+  },
+  preflight: {
+    // Import external stylesheet
+    '@import': `url('https://fonts.googleapis.com/css2?family=Inconsolata:wght@200;300;400;500;600;700;800;900&display=swap')`
+  }
+}, (data) => <App {...data} />)
 
 hydrate(<App />);
 
